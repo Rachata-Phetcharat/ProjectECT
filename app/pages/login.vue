64022919b8f5c5cbf001c088b7191525d47fdf01
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2025-09-01 18:17:15 +0700

    new commit

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -17,9 +17,9 @@
                 <h3>ICIT Account</h3>
                 <input type="text" placeholder="" required
                     class="border border-gray-300 rounded-lg px-4 py-2 mb-4 w-full" />
-                <h3>Password</h3>
+                <!-- <h3>Password</h3>
                 <input type="password" placeholder="" required
-                    class="border border-gray-300 rounded-lg px-4 py-2 mb-4 w-full" />
+                    class="border border-gray-300 rounded-lg px-4 py-2 mb-4 w-full" /> -->
                 <div class="text-center">
                     <button type="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
