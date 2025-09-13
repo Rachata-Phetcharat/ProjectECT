cd39b525d38211837188dd0978f34168e6f2ce6b
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2025-09-13 20:15:31 +0700

    เพิ่มหน้าการแชนแนล

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,7 +2,7 @@
     <div class="mt-4 absolute top-0 right-0 p-4">
         <a href="/">
             <button type="button"
-                class="bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
+                class="bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none cursor-pointer">
                 <div class="flex items-center gap-2 text-white">
                     <Icon name="line-md:home" class="w-6 h-6" />
                     Home
@@ -14,7 +14,7 @@
     <div>
         <form action="assets/main.py" method="post"
             class="max-w-md mx-auto mt-10 p-6 border border-gray-300 rounded-lg shadow-md">
-            <div class="text-center bg-neutral-500 text-white p-6 rounded-lg">
+            <div class="text-center bg-gray-800 text-white p-6 rounded-lg">
                 <h1>เข้าระบบด้วย ICIT Account</h1>
             </div>
             <div class="mt-6">
@@ -26,7 +26,7 @@
                     class="border border-gray-300 rounded-lg px-4 py-2 mb-4 w-full" /> -->
                 <div class="text-center">
                     <button type="submit"
-                        class="bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
+                        class="bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none cursor-pointer">
                         <div class="flex items-center gap-2 text-white">
                             <Icon name="mdi:login" class=" w-6 h-6" />
                             เข้าสู่ระบบ
